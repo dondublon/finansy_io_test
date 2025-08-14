9b30aa2fce588bb2eac5cfa169e350195e1de74a
Yaroslav Benin <dondublon1@yandex.ru>
2025-08-14 19:37:23 +0300

    custom validation, to remove the slashh

--- src/schemas.py
+++ src/schemas.py
@@ -1,7 +1,16 @@
-from pydantic import BaseModel, AnyHttpUrl
+from urllib.parse import urlparse
+from pydantic import BaseModel, AnyHttpUrl, field_validator
+
 
 class LinkCreate(BaseModel):
-    url: AnyHttpUrl
+    url: str
+
+    @field_validator("url")
+    def validate_url(cls, v: str) -> str:
+        parsed = urlparse(v)
+        if not parsed.scheme or not parsed.netloc:
+            raise ValueError("Invalid URL")
+        return v
 
 class LinkCreateResponse(BaseModel):
     short_key: str
